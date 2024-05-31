37d7949e5423b6bf4f2f7c22850e2e7b60431877
hushlovies <canariakaira@gmail.com>
2024-05-31 02:28:22 +0200

    otherfront modifications

--- frontend/src/views/Miscellaneous.vue
+++ frontend/src/views/Miscellaneous.vue
@@ -1,100 +1,54 @@
-<template>
-  <div class="container mx-auto mt-10">
-    <h1>Miscellaneous HERE</h1>
-    <h2 class="text-2xl font-bold mb-4">Countries</h2>
-    
-    <table class="min-w-full table-auto">
-      <thead class="bg-gray-800 text-white">
-        <tr>
-          <th class="px-4 py-2">Country Name</th>
-          <th class="px-4 py-2">3 Letter Code</th>
-          <th class="px-4 py-2">Gold Medal</th>
-          <th class="px-4 py-2">Silver Medal</th>
-          <th class="px-4 py-2">Bronze Medal</th>
-          <th class="px-4 py-2">No. of ahtletes</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="country in countries" :key="country.country_3_letter_code" class="bg-white  text-black border-b">
-          <td class="px-4 py-2">{{ country.country_name }}</td>
-          <td class="px-4 py-2">{{ country.country_3_letter_code }}</td>
-          <td class="px-4 py-2">{{ country.total_gold }}</td>
-          <td class="px-4 py-2">{{ country.total_silver }}</td>
-          <td class="px-4 py-2">{{ country.total_bronze }}</td>
-          <td class="px-4 py-2">{{ country.athlete_count }}</td>
-        </tr>
-        <tr v-if="countries.length === 0" class="bg-white border-b">
-          <td class="px-4 py-2" colspan="2">No data available</td>
-        </tr>
-      </tbody>
-    </table>
-    <!-- Ajout des boutons de pagination ici -->
-    <div class="flex justify-center my-4">
-  <button @click="goToFirstPage" :disabled="currentPage === 1" class="mx-2 p-2 border rounded">First</button>
-  <button @click="changePage(currentPage - 1)" :disabled="currentPage <= 1" class="mx-2 p-2 border rounded">Previous</button>
-  <button @click="changePage(currentPage + 1)" :disabled="currentPage >= totalPages" class="mx-2 p-2 border rounded">Next</button>
-</div>
 
+<!-- <div class="container bg-white text-black mx-auto mt-10"> -->
+<template>
+  <div class="container bg-white text-black mx-auto mt-10">
+    <h1 class="text-2xl font-bold mb-4">Fait Divers</h1>
+  <div class="m-20 text-left">
+
+    <p class="paraTitle" >1. La France a organisé 6 JO : 3 d’hiver et 3 d’été (en comptant celui de 2024) ?</p>
+    <p>Oui</p>
+    
+    <p class="paraTitle">2. La France est le 2è pays qui a organisé le plus de JO après les USA (8 JO) ? </p>
+    <p>La France est le deuxième pays qui organise le plus de JO</p>
+    <p class="paraTitle">3. Les JO d’hiver sont nés à Chamonix en 1924 ?</p>
+    <p>Oui</p>
+    <p class="paraTitle">4. JO de Paris, en 1900 : les femmes peuvent participer aux JO ?</p>
+    <p>Les femmes ont participé aux Jeux Olympiques de Paris en 1900.</p>
+    <p class="paraTitle">5. Seuls 4 athlètes ont remporté des médailles à la fois aux JO d’hiver et d’été. Une seule d’entre eux, Christa Ludinger-Rothenburger, a remporté des médailles au cours de la même année ?</p>
+    <p>Oui, 4 athlètes ont remporté des médailles aux Jeux Olympiques au cours des deux saisons. Oui |Christa LUDING-ROTHENBURGER a gagné à la fois en été et en hiver en 1988</p>
+    <p class="paraTitle">6. De 1924 à 1992, les JO d’hiver et d’été avaient lieu au cours de la même année. Désormais, ils sont organisés selon des cycles distincts avec une alternance de 2 ans ?</p>
+    <p></p>
+    <p class="paraTitle">7. Tarzan lui-même a participé aux JO. En effet, Johnny Weissmuller, ancien athlète devenu acteur et ayant interprété 12 films de Tarzan, a remporté 5 médailles d’or en natation dans les années 1920 ?</p>
+    <p>Johnny Weissmuller a remporté 3 médailles d'or en natation.</p>
+    <p class="paraTitle">8. Les JO de Londres de 2012 ont été les 1ers JO durant lesquels tous les pays participants ont envoyé des athlètes de sexe féminin ?</p>
+    <p>Non</p>
+    <p class="paraTitle">9. Les sports suivants ne font (malheureusement) plus partie des J.O : la natation synchronisée en solo, le tir à la corde, la corde à grimper, la montgolfière, le duel au pistolet, le vélo tandem, la course d’obstacles à la nage et le plongeon à distance. Par chance, le tir au pigeon n’a été mis en place qu’une seule fois pendant les Jeux Olympiques de Paris de 1900 ?</p>
+    <p>Le sport 'natation synchronisée en solo' n'a pas été disputé aux Jeux Olympiques de Paris en 1900.
+Le sport 'tir à la corde' n'a pas été disputé aux Jeux Olympiques de Paris en 1900.
+Le sport 'plongeon à distance' n'a pas été disputé aux Jeux Olympiques de Paris en 1900.
+Le sport 'tir au pigeon' n'a pas été disputé aux Jeux Olympiques de Paris en 1900.</p>
+    <p class="paraTitle">10. Les Jeux de 2016, à Rio, marqueront la 1è manifestation des JO en Amérique du Sud ?</p>
+    <p>Oui</p>
+    <p class="paraTitle">11. Pendant les 17 jours des JO d’été de 2016, 10 500 athlètes, originaires de 205 pays, représenteront 42 sports différents et participeront à 306 épreuves, à Rio ?</p>
+    <p>non , pendant les JO d'été 2016 5908 athletes, originaires de 199 pays, répresentants 41 sports ont participé a 289 épreuves.</p>
   </div>
+  <br><br>
+</div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      countries: [],
-      currentPage: 1,
-      perPage: 25,
-      totalPages: 0,
-    };
-  },
-  methods: {
-    fetchCountries() {
-      const params = new URLSearchParams({
-        page: this.currentPage,
-        per_page: this.perPage,
-      }).toString();
-      
-      fetch(`http://localhost:5000/countries?${params}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.data) {
-            this.countries = data.data;
-            this.totalPages = Math.ceil(data.total / this.perPage); // Ensure your backend returns the total count
-          } else {
-            this.countries = [];
-            this.totalPages = 0;
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-          this.countries = [];
-          this.totalPages = 0;
-        });
-    },
-    changePage(page) {
-      if (page < 1 || page > this.totalPages) return;
-      this.currentPage = page;
-      this.fetchCountries();
-    },
-    goToFirstPage() {
-      if (this.currentPage !== 1) {
-        this.currentPage = 1;
-        this.fetchCountries();
-      }
-    }
-  },
-  mounted() {
-    this.fetchCountries();
-  },
-  watch: {
-    perPage(newPerPage, oldPerPage) {
-      // Reset current page when changing items per page
-      if (newPerPage !== oldPerPage) {
-        this.currentPage = 1;
-        this.fetchCountries();
-      }
-    },
-  },
-};
-</script>
+<style>
+.smTitle{
+  font-weight: bold;
+  font-size: 30px;
+}
+ul li{
+ margin-left: 10px; 
+}
+.paraTitle{
+  color:#EDA62D;
+  font-size: 15px;
+  font-weight: 700;
+  margin: 10px 0;
+  padding-top: 10px;
+}
+</style>
